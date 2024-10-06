7b0117da96533350b96d310db6fb5e3ff0e0aea8
Anthony Ajra <143861597+AnthoLogy63@users.noreply.github.com>
2024-10-05 23:01:50 -0500

    Animaciones y movimineto incluidos

--- Exoplanets Delivery/Assets/Scripts/Movimiento.cs
+++ Exoplanets Delivery/Assets/Scripts/Movimiento.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    private float movimientoFuerza = 5f;
-    private float fuerzaSalto = 12f;
+    private float movimientoFuerza = 3.5f;
+    private float fuerzaSalto = 6f;
     private Rigidbody2D miCuerpoRigido;
+
+    public Animator animator;
 
     void Start()
     {
         miCuerpoRigido = GetComponent<Rigidbody2D>();
+
+        // Configura la fricción a cero
+        Collider2D collider = GetComponent<Collider2D>();
+        PhysicsMaterial2D noFrictionMaterial = new PhysicsMaterial2D();
+        noFrictionMaterial.friction = 0f;
+        noFrictionMaterial.bounciness = 0f;
+        collider.sharedMaterial = noFrictionMaterial;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float movementX = Input.GetAxisRaw("Horizontal");
         miCuerpoRigido.velocity = new Vector2(movementX * movimientoFuerza, miCuerpoRigido.velocity.y);
 
-        // Detectar entrada de salto
+        animator.SetFloat("Movement", Mathf.Abs(movementX * movimientoFuerza));
+
+        if (movementX < 0)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (movementX > 0)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
         if (Input.GetButtonDown("Jump") && Mathf.Abs(miCuerpoRigido.velocity.y) < 0.001f)
         {
             miCuerpoRigido.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
